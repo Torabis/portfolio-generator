d37d14ee89f6587720fdb4181b3911a429cabe57
Torabis <sadihome@gmail.com>
2020-09-19 21:54:27 -0700

    question validation

--- app.js
+++ app.js
@@ -2,21 +2,44 @@
 const inquirer = require('inquirer');
 const promptUser = () => {
   return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name? (Required)',
+          validate: nameInput => {
+              if (nameInput) {
+                  return true;
+              } else {
+                  console.log('Please enter your name!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your Github Username (Required)',
+          validate: gitHubInput => {
+              if (gitHubInput) {
+                  return true;
+              } else {
+                  console.log('Please enter your name!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
+      },
+      {
+          type: 'input',
+          name: 'about',
+          message: 'Provide some information about yourself:',
+          when: ({ confirmAbout }) => confirmAbout
+      },
   ]);
 };
 
@@ -30,6 +53,7 @@ const promptProject = portfolioData => {
   =================
   `);
   return inquirer.prompt([
+    
     {
       type: 'input',
       name: 'name',
